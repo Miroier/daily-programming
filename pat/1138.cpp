86bd119f76955dcb7b7ebaaa4f620857d289a062
Miroier <2606381565@qq.com>
2021-03-08 20:29:29 +0800

    3/8

---
+++ pat/1138.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "vector"
+using namespace std;
+vector<int> in, pre, post;
+void getPost(int preL, int preR, int inL, int inR)
+{
+    if (preL > preR)
+        return;
+    int i = inL;
+    while (i < inR && in[i] != pre[preL])
+        i++;
+    getPost(preL + 1, preL + (i - inL), inL, i - 1);
+    getPost(preL + (i - inL) + 1, preR, i + 1, inR);
+    post[preR] = pre[preL];
+}
+int main()
+{
+#ifdef ONLINE_JUDGE
+#else
+    freopen("input.txt", "r", stdin);
+#endif
+    int n;
+    cin >> n;
+    pre.resize(n + 1);
+    in.resize(n + 1);
+    post.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >>
+            pre[i];
+    for (int i = 1; i <= n; i++)
+        cin >> in[i];
+    getPost(1, n, 1, n);
+    cout << post[1];
+}
