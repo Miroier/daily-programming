bf2ad002dbd9fc5517f8736e2e1e98032e9c6b67
Miroier <2606381565@qq.com>
2021-03-11 11:05:17 +0800

    rename

--- pat/1152.cpp
+++ pat/1152.cpp
@@ -1,115 +1,17 @@
-#include "algorithm"
 #include "iostream"
-#include "map"
-#include "vector"
 using namespace std;
-/*
-    输出时间时要用%06d，否则错两个点
- */
-struct peo
+bool check(long long x)
 {
-    string card;
-    char level;
-    int testSiteNumber;
-    int testDate;
-    int testee;
-    int score;
-};
-int n, m;
-vector<peo> v;
-void inp()
-{
-    cin >> n >> m;
-    string s;
-    char level;
-    int testSiteNumber;
-    int testDate;
-    int testee;
-    int score;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> s >> score;
-        level = s[0];
-        testSiteNumber = stoi(s.substr(1, 3));
-        testDate = stoi(s.substr(4, 6));
-        testee = stoi(s.substr(10));
-        v.push_back({s, level, testSiteNumber, testDate, testee, score});
-    }
-}
-bool cmp1(peo &a, peo &b)
-{
-    if (a.score != b.score)
-        return a.score > b.score;
-    return a.card < b.card;
-}
-bool cmp3(pair<int, int> a, pair<int, int> b)
-{
-    if (a.second != b.second)
-        return a.second > b.second;
-    return a.first < b.first;
-}
-void work()
-{
-    int type;
-    for (int i = 1; i <= m; i++)
-    {
-        cin >> type;
-        vector<peo> tmp;
-        if (type == 1)
-        {
-            char level;
-            cin >> level;
-            for (auto x : v)
-                if (x.level == level)
-                    tmp.push_back(x);
-            printf("Case %d: %d %c\n", i, type, level);
-            if (tmp.size() == 0)
-            {
-                cout << "NA" << endl;
-                continue;
-            }
-            sort(tmp.begin(), tmp.end(), cmp1);
-            for (auto x : tmp)
-                printf("%s %d\n", x.card.c_str(), x.score);
-        }
-        else if (type == 2)
-        {
-            int testSiteNumber;
-            cin >> testSiteNumber;
-            int cnt = 0, sum = 0;
-            for (auto x : v)
-                if (x.testSiteNumber == testSiteNumber)
-                    cnt++, sum += x.score;
-            printf("Case %d: %d %03d\n", i, type, testSiteNumber);
-            if (cnt == 0)
-            {
-                cout << "NA" << endl;
-                continue;
-            }
-            printf("%d %d\n", cnt, sum);
-        }
-        else if (type == 3)
-        {
-            map<int, int> mp;
-            int testDate;
-            cin >> testDate;
-            for (auto x : v)
-                if (x.testDate == testDate)
-                    mp[x.testSiteNumber]++;
-            vector<pair<int, int>> tmp;
-            for (auto x : mp)
-                tmp.push_back(x);
-            printf("Case %d: %d %06d\n", i, type, testDate);
-            if (tmp.size() == 0)
-            {
-                cout << "NA" << endl;
-                continue;
-            }
-            sort(tmp.begin(), tmp.end(), cmp3);
-            for (auto x : tmp)
-                printf("%03d %d\n", x.first, x.second);
-        }
-    }
+    if (x <= 1)
+        return false;
+    if (x == 2)
+        return true;
+    if (x % 2 == 0)
+        return false;
+    for (long long i = 3; i * i <= x; i += 2)
+        if (x % i == 0)
+            return false;
+    return true;
 }
 int main()
 {
@@ -117,6 +19,27 @@ int main()
 #else
     freopen("input.txt", "r", stdin);
 #endif
-    inp();
-    work();
+    string s, tmp;
+    int l, k;
+    cin >> l >> k;
+    cin >> s;
+    bool flag = 0;
+    if (l < k) //最后一个点
+    {
+        cout << 404 << endl;
+        return 0;
+    }
+    for (int i = 0; i <= s.size() - k; i++)
+    {
+        tmp = s.substr(i, k);
+        if (check(stoll(tmp)))
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag)
+        cout << tmp << endl;
+    else
+        cout << 404 << endl;
 }
