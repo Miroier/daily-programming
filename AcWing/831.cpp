91caf398867af44edd1e29ceb6fa1b68c0be4f8b
Miroier <2606381565@qq.com>
2021-04-05 16:53:17 +0800

    4/5

---
+++ AcWing/831.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+using namespace std;
+const int N = 100010, M = 1000010;
+int n, m;
+char p[N], s[M];
+int ne[N];
+int main()
+{
+    cin >> n >> p + 1 >> m >> s + 1;
+    // 求next数组过程
+    ne[1] = 0;
+    for (int i = 2, j = 0; i <= n; i++)
+    {
+        while (j && p[i] != p[j + 1])
+            j = ne[j];
+        if (p[i] == p[j + 1])
+            j++;
+        ne[i] = j;
+    }
+    // kmp匹配过程
+    for (int i = 1, j = 0; i <= m; i++)
+    {
+        while (j && s[i] != p[j + 1])
+            j = ne[j];
+        if (s[i] == p[j + 1])
+            j++;
+        if (j == n)
+        {
+            //匹配成功
+            cout << i - n << " ";
+            j = ne[j];
+        }
+    }
+}
