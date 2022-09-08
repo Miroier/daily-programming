a72dbb519d8264f00032f88e4304970a107aedc7
Miroier <2606381565@qq.com>
2022-09-09 00:22:36 +0800

    220908

--- AcWing/795.cpp
+++ AcWing/795.cpp
@@ -1,19 +1,19 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
+
+const int N = 1e5 + 10;
+
+int a[N];
 int n, m;
-int a[100005];
+int l, r;
+
 int main()
 {
-#ifdef ONLINE_JUDGE
-#else
-    freopen("input.txt", "r", stdin);
-#endif
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
         cin >> a[i], a[i] += a[i - 1];
     while (m--)
     {
-        int l, r;
         cin >> l >> r;
         cout << a[r] - a[l - 1] << endl;
     }
--- AcWing/798.cpp
+++ AcWing/798.cpp
@@ -1,35 +1,30 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
 const int N = 1005;
-int a[N][N], diff[N][N];
+int a[N][N], b[N][N];
+int n, m, q;
+int x1, y1, x2, y2, c;
 int main()
 {
-#ifdef ONLINE_JUDGE
-#else
-    freopen("input.txt", "r", stdin);
-#endif
-    int n, m, q;
     cin >> n >> m >> q;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
             cin >> a[i][j];
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            diff[i][j] = a[i][j] - a[i - 1][j] - a[i][j - 1] + a[i - 1][j - 1];
-    fill(a[0], a[0] + N * N, 0);
+            b[i][j] = a[i][j] - a[i - 1][j] - a[i][j - 1] + a[i - 1][j - 1];
     while (q--)
     {
-        int x1, x2, y1, y2, c;
         cin >> x1 >> y1 >> x2 >> y2 >> c;
-        diff[x1][y1] += c;
-        diff[x2 + 1][y1] -= c;
-        diff[x1][y2 + 1] -= c;
-        diff[x2 + 1][y2 + 1] += c;
+        b[x1][y1] += c;
+        b[x1][y2 + 1] -= c;
+        b[x2 + 1][y1] -= c;
+        b[x2 + 1][y2 + 1] += c;
     }
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            a[i][j] = diff[i][j] + a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1];
+            b[i][j] += b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            cout << a[i][j] << (j < m ? " " : "\n");
+            cout << b[i][j] << (j < m ? " " : "\n");
 }
