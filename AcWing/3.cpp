eee71f039c4c61023cbf28800b365c543157398c
Miroier <2606381565@qq.com>
2022-09-22 00:26:56 +0800

    220921

--- AcWing/3.cpp
+++ AcWing/3.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 const int N = 1005;
-
 int n, m;
 int v[N], w[N];
-int f[N]; // f[i]只用到了f[i-1]，可以用滚动数组，又因为f[j]用到的f[j-v[i]]是第i-1层的，不是第i层的，所以应该倒序遍历
+int f[N]; // f[i]只用到了f[i-1]，可以用滚动数组，和01背包不同的是，因为f[j]用到的f[j-v[i]]是第i层的，所以正序遍历j
 
 int main()
 {
@@ -14,7 +13,7 @@ int main()
         cin >> v[i] >> w[i];
 
     for (int i = 1; i <= n; i++)
-        for (int j = m; j >= v[i]; j--)
+        for (int j = v[i]; j <= m; j++)
             f[j] = max(f[j], f[j - v[i]] + w[i]);
 
     cout << f[m];
